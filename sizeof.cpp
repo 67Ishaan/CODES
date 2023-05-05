880c8dbb4fc78eabea1dc23671d9113c27f80e91
67Ishaan <118922359+67Ishaan@users.noreply.github.com>
2023-05-05 21:33:22 +0530

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<" size of int :"<<sizeof(int);
+    cout<<" size of float :"<<sizeof(float);
+    cout<<" size of double :"<<sizeof(double);
+    cout<<" size of char :"<<sizeof(char);
+}
